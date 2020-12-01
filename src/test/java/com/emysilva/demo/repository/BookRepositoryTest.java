335c462a2faf510f09e21a8453ae5ae231671c64
chukwuebuka <emysly12@gmail.com>
2020-12-01 13:55:31 +0100

    create book repository test

---
+++ src/test/java/com/emysilva/demo/repository/BookRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.emysilva.demo.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BookRepositoryTest {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Test
+    public void testCreateBook() {
+
+    }
+
+    @Test
+    public void testListBooks() {
+    }
+
+    @Test
+    public void testUpdateBook() {
+    }
+
+    @Test
+    public void testDeleteBook() {
+    }
+}
