94e0c5c7dbd07f226e0925df9ff63a4bf8db7e13
chukwuebuka <emysly12@gmail.com>
2020-12-01 13:55:02 +0100

    create book repository

---
+++ src/main/java/com/emysilva/demo/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.emysilva.demo.repository;
+
+import com.emysilva.demo.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
